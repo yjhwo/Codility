092475fc4ec509cc43b72a16601f3856e94ab431
estsoft <estsoft@192.168.230.21>
2016-06-30 12:52:46 +0900

    lesson03_complete

--- src/lesson03.java
+++ src/lesson03.java
@@ -3,7 +3,8 @@ public class lesson03 {
 
 	public static void main(String[] args) {
 		int[] A = {9,3,9,3,9,7,9};
-		System.out.println(solution(A));
+		
+		System.out.println(solution(A));		
 	}
 
 	public static int solution(int[] A) {
